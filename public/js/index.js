5e0f4b1be26482edc3ad9befd2ded6797008cb89
Pawan <Pawankumar.vandemataram@gmail.com>
2022-07-14 02:03:52 +0530

    file sharing app

--- public/js/index.js
+++ public/js/index.js
@@ -18,9 +18,9 @@ const toast = document.querySelector(".toast");
 
 // const APP_BASE_URL = "http://localhost:3000";
 const APP_BASE_URL = "pawan-metashare.herokuapp.com"
-// const host = APP_BASE_URL;
-// const uploadURL = `${host}/api/files`;
-const uploadURL = `/api/files`;
+const host = APP_BASE_URL;
+const uploadURL = `${host}/api/files`;
+// const uploadURL = `/api/files`;
 
 const maxAllowedSize = 100 * 1024 * 1024;
 
